84255c49cfda80e60e655ba609dbbdaf89c1a64f
CharlesLove <crashbanito@hotmail.com>
2023-12-04 16:59:52 +1300

    Massive day 03 clippy clean

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,10 +1,10 @@
 #![warn(clippy::pedantic)]
-use std::{collections::HashMap, fs, iter::Product};
+use std::{collections::HashMap, fs};
 
 #[derive(Eq, Hash, PartialEq, Clone, Copy)]
 struct Coords2D {
-    x: usize,
-    y: usize,
+    x: u32,
+    y: u32,
 }
 
 fn main() {
@@ -17,15 +17,15 @@ fn main() {
     println!("Part 2:\n{}", find_gear_ratio_sum(input));
 }
 
-fn get_width(input: &str) -> usize {
-    input.lines().next().unwrap().len()
+fn get_width(input: &str) -> i32 {
+    input.lines().next().unwrap().len().try_into().unwrap()
 }
 
-fn get_height(input: &str) -> usize {
-    input.lines().count()
+fn get_height(input: &str) -> i32 {
+    input.lines().count().try_into().unwrap()
 }
 
-fn check_surrounding(input: &str, cur_x: usize, cur_y: usize, length: usize) -> bool {
+fn check_surrounding(input: &str, cur_x: i32, cur_y: i32, length: i32) -> bool {
     let mut is_summable = false;
     let mut surrounding_string = String::new();
     let mut top_bound = 0;
@@ -48,7 +48,13 @@ fn check_surrounding(input: &str, cur_x: usize, cur_y: usize, length: usize) -> 
     for y in top_bound..=bottom_bound {
         for x in left_bound..=right_bound {
             //let cur_line = input.lines().nth(y).unwrap();
-            let cur_char = input.lines().nth(y).unwrap().chars().nth(x).unwrap();
+            let cur_char = input
+                .lines()
+                .nth(y.try_into().unwrap())
+                .unwrap()
+                .chars()
+                .nth(x.try_into().unwrap())
+                .unwrap();
             surrounding_string.push(cur_char);
 
             if cur_char.is_ascii_punctuation() && cur_char != '.' {
@@ -78,7 +84,12 @@ fn find_numbers_sum(input: &str) -> u32 {
             if ch.is_ascii_digit() {
                 cur_num_string.push(ch);
             } else if !cur_num_string.is_empty() {
-                if check_surrounding(input, cur_x, cur_y, cur_num_string.len()) {
+                if check_surrounding(
+                    input,
+                    cur_x,
+                    cur_y,
+                    cur_num_string.len().try_into().unwrap(),
+                ) {
                     sum += &cur_num_string.parse().unwrap();
                 }
                 cur_num_string = String::new();
@@ -86,7 +97,12 @@ fn find_numbers_sum(input: &str) -> u32 {
             cur_x += 1;
         }
         if !cur_num_string.is_empty() {
-            if check_surrounding(input, cur_x, cur_y, cur_num_string.len()) {
+            if check_surrounding(
+                input,
+                cur_x,
+                cur_y,
+                cur_num_string.len().try_into().unwrap(),
+            ) {
                 sum += &cur_num_string.parse().unwrap();
             }
             cur_num_string = String::new();
@@ -112,7 +128,12 @@ fn find_gear_ratio_sum(input: &str) -> u32 {
             if ch.is_ascii_digit() {
                 cur_num_string.push(ch);
             } else if !cur_num_string.is_empty() {
-                for location in get_gear_locations(input, cur_x, cur_y, cur_num_string.len()) {
+                for location in get_gear_locations(
+                    input,
+                    cur_x,
+                    cur_y,
+                    cur_num_string.len().try_into().unwrap(),
+                ) {
                     gear_hash
                         .entry(location)
                         .or_default()
@@ -126,12 +147,19 @@ fn find_gear_ratio_sum(input: &str) -> u32 {
             cur_x += 1;
         }
         if !cur_num_string.is_empty() {
-            for location in get_gear_locations(input, cur_x, cur_y, cur_num_string.len()) {
+            get_gear_locations(
+                input,
+                cur_x,
+                cur_y,
+                cur_num_string.len().try_into().unwrap(),
+            )
+            .into_iter()
+            .for_each(|location| {
                 gear_hash
                     .entry(location)
                     .or_default()
                     .push(cur_num_string.parse().unwrap());
-            }
+            });
             cur_num_string = String::new();
         }
         cur_y += 1;
@@ -146,7 +174,7 @@ fn find_gear_ratio_sum(input: &str) -> u32 {
     sum
 }
 
-fn get_gear_locations(input: &str, cur_x: usize, cur_y: usize, length: usize) -> Vec<Coords2D> {
+fn get_gear_locations(input: &str, cur_x: i32, cur_y: i32, length: i32) -> Vec<Coords2D> {
     let _has_gear = false;
     let mut surrounding_string = String::new();
     let mut top_bound = 0;
@@ -161,22 +189,31 @@ fn get_gear_locations(input: &str, cur_x: usize, cur_y: usize, length: usize) ->
         left_bound = cur_x - length - 1;
     }
     let mut right_bound = get_width(input) - 1;
-    if cur_x < get_width(input) {
+    if cur_x < get_width(input).try_into().unwrap() {
         right_bound = cur_x;
     }
     let mut bottom_bound = get_height(input) - 1;
-    if cur_y + 1 < get_height(input) {
+    if cur_y + 1 < get_height(input).try_into().unwrap() {
         bottom_bound = cur_y + 1;
     }
 
     for y in top_bound..=bottom_bound {
         for x in left_bound..=right_bound {
             //let cur_line = input.lines().nth(y).unwrap();
-            let cur_char = input.lines().nth(y).unwrap().chars().nth(x).unwrap();
+            let cur_char = input
+                .lines()
+                .nth(y.try_into().unwrap())
+                .unwrap()
+                .chars()
+                .nth(x.try_into().unwrap())
+                .unwrap();
             surrounding_string.push(cur_char);
 
             if cur_char == '*' {
-                gear_locations.push(Coords2D { x, y });
+                gear_locations.push(Coords2D {
+                    x: x.try_into().unwrap(),
+                    y: y.try_into().unwrap(),
+                });
             }
         }
         surrounding_string.push('\n');
