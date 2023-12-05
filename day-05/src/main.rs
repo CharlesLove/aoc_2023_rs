4702d5b87ba1ac95307657a94c81f790f6e126c7
CharlesLove <crashbanito@hotmail.com>
2023-12-06 08:27:03 +1300

    Start day-05

---
+++ day-05/src/main.rs
@@ -0,0 +1,32 @@
+#![warn(clippy::pedantic)]
+use std::fs;
+
+fn main() {
+    let binding = fs::read_to_string("./day-05/inputs/input.txt").unwrap();
+    let input = binding.trim();
+
+    println!("Part 1:\n{}", part_one(input));
+    println!("Part 2:\n{}", part_two(input));
+}
+
+fn part_one(input: &str) -> u32 {
+    0
+}
+fn part_two(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_LINES1: &str = r"";
+
+    #[test]
+    fn test_one() {
+        assert_eq!(part_one(TEST_LINES1), 8);
+    }
+    #[test]
+    fn test_two() {
+        assert_eq!(part_two(TEST_LINES1), 8);
+    }
+}
