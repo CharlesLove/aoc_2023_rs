f508385d16ae31c198d8b8a7f4ec314ac07b244f
CharlesLove <crashbanito@hotmail.com>
2023-12-06 10:33:07 +1300

    Get seeds and map vectors

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,9 +1,18 @@
 #![warn(clippy::pedantic)]
 use std::fs;
 
+#[derive(PartialEq, Debug)]
+struct AlmanacMap {
+    destination_range_start: u32,
+    source_range_start: u32,
+    range_length: u32,
+}
+
 fn main() {
     let binding = fs::read_to_string("./day-05/inputs/input.txt").unwrap();
     let input = binding.trim();
+
+    println!("{}", input);
 
     println!("Part 1:\n{}", part_one(input));
     println!("Part 2:\n{}", part_two(input));
@@ -16,14 +25,130 @@ fn part_two(input: &str) -> u32 {
     0
 }
 
+fn get_seeds(input: &str) -> Vec<u32> {
+    //let mut seed_vec: Vec<u32> = Vec::new();
+
+    let seed_string = input
+        .split("seeds:")
+        .nth(1)
+        .unwrap()
+        .split("\n\n")
+        .next()
+        .unwrap()
+        .trim();
+    let seed_vec = seed_string
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    seed_vec
+}
+
+fn get_map(input: &str, map_name: &str) -> Vec<AlmanacMap> {
+    let mut map_vec: Vec<AlmanacMap> = Vec::new();
+    let map_name_formatted = format!("{map_name} map:");
+    let map_string = input
+        .split(&map_name_formatted)
+        .nth(1)
+        .unwrap()
+        .split("\n\n")
+        .next()
+        .unwrap()
+        .trim();
+
+    for line in map_string.lines() {
+        let number: Vec<u32> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let almanac_map_entry: AlmanacMap = AlmanacMap {
+            destination_range_start: number[0],
+            source_range_start: number[1],
+            range_length: number[2],
+        };
+        map_vec.push(almanac_map_entry);
+    }
+
+    map_vec
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST_LINES1: &str = r"";
+    const TEST_LINES1: &str = r"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
 
     #[test]
     fn test_one() {
         assert_eq!(part_one(TEST_LINES1), 8);
+    }
+    #[test]
+    fn test_seeds() {
+        assert_eq!(get_seeds(TEST_LINES1), [79, 14, 55, 13]);
+    }
+    #[test]
+    fn test_maps() {
+        assert_eq!(
+            get_map(TEST_LINES1, "seed-to-soil"),
+            [
+                AlmanacMap {
+                    destination_range_start: 50,
+                    source_range_start: 98,
+                    range_length: 2
+                },
+                AlmanacMap {
+                    destination_range_start: 52,
+                    source_range_start: 50,
+                    range_length: 48
+                },
+            ]
+        );
+        assert_eq!(
+            get_map(TEST_LINES1, "humidity-to-location"),
+            [
+                AlmanacMap {
+                    destination_range_start: 60,
+                    source_range_start: 56,
+                    range_length: 37
+                },
+                AlmanacMap {
+                    destination_range_start: 56,
+                    source_range_start: 93,
+                    range_length: 4
+                },
+            ]
+        );
     }
     #[test]
     fn test_two() {
