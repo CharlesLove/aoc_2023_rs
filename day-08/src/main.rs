7f5cfa6123beae6f230d5cb79da3071d15ee353d
CharlesLove <crashbanito@hotmail.com>
2023-12-09 08:56:56 +1300

    Part 2 test passes

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -24,7 +24,7 @@ fn part_one(input: &str) -> u32 {
 
         let two_halves: Vec<&str> = current_line.split(" = ").collect();
 
-        let current_location = two_halves[0].to_string();
+        let location_half = two_halves[0].to_string();
 
         let destinations: Vec<String> = two_halves[1]
             .to_string()
@@ -38,7 +38,7 @@ fn part_one(input: &str) -> u32 {
         destination_map.insert('L', destinations[0].to_string());
         destination_map.insert('R', destinations[1].to_string());
 
-        location_map.insert(current_location, destination_map);
+        location_map.insert(location_half, destination_map);
     }
 
     let mut i = 0;
@@ -63,7 +63,66 @@ fn part_one(input: &str) -> u32 {
     steps
 }
 fn part_two(input: &str) -> u32 {
-    0
+    let mut steps = 0;
+    //let mut current_location = "AAA";
+    let mut current_locations: Vec<String> = Vec::new();
+    let final_destination = "ZZZ";
+    let mut found = false;
+    let instructions: Vec<char> = input.lines().next().unwrap().chars().collect();
+
+    let mut location_map: HashMap<String, HashMap<char, String>> = HashMap::new();
+
+    // build mega hash map
+    for line_num in 2..input.lines().count() {
+        let current_line = input.lines().nth(line_num).unwrap();
+
+        let two_halves: Vec<&str> = current_line.split(" = ").collect();
+
+        let location_half = two_halves[0].to_string();
+        if location_half.ends_with('A') {
+            current_locations.push(location_half.clone());
+        }
+
+        let destinations: Vec<String> = two_halves[1]
+            .to_string()
+            .replace(['(', ')'], "")
+            .split(", ")
+            .map(str::to_string)
+            .collect();
+
+        let mut destination_map: HashMap<char, String> = HashMap::new();
+
+        destination_map.insert('L', destinations[0].to_string());
+        destination_map.insert('R', destinations[1].to_string());
+
+        location_map.insert(location_half, destination_map);
+    }
+
+    let mut i = 0;
+    let mut finds = 0;
+    while finds != current_locations.len() {
+        finds = 0;
+        steps += 1;
+
+        for l in 0..current_locations.len() {
+            let current_instruction = instructions[i];
+
+            let next_location = &location_map[&current_locations[l]][&current_instruction];
+
+            current_locations[l] = next_location.to_string();
+            if current_locations[l].ends_with('Z') {
+                finds += 1;
+            } else {
+                break;
+            }
+        }
+        i += 1;
+        if i >= instructions.len() {
+            i = 0;
+        }
+    }
+
+    steps
 }
 
 #[cfg(test)]
@@ -91,6 +150,6 @@ ZZZ = (ZZZ, ZZZ)";
     }
     #[test]
     fn test_two() {
-        assert_eq!(part_two(TEST_LINES1), 8);
+        assert_eq!(part_two(TEST_LINES2), 6);
     }
 }
